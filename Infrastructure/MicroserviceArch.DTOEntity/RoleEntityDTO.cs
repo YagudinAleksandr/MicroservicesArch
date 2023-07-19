a6a76a87a62b38b351481e226387ab61db4a4772
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 23:35:03 +0300

    DTO модель ролей

---
+++ Infrastructure/MicroserviceArch.DTOEntity/RoleEntityDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroserviceArch.DTOEntity
+{
+    /// <summary>
+    /// DTO модель роли
+    /// </summary>
+    public class RoleEntityDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле с названием должно быть заполнено")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле с названием должно быть заполнено")]
+        public string Description { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Notification { get; set; }
+        public bool IsSuccesful { get; set; }
+    }
+}
