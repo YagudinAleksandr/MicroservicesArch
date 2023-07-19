1bd720b5a43b500b3ebd24cb6ced295449565f08
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 16:50:43 +0300

    Контекст базы данных

---
+++ Data/MicroserviceArch.DAL/Context/DataDB.cs
@@ -0,0 +1,31 @@
+﻿using MicroserviceArch.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroserviceArch.DAL.Context
+{
+    public class DataDB : DbContext
+    {
+        public DataDB(DbContextOptions<DataDB> options) : base(options) { }
+
+        /// <summary>
+        /// Роли
+        /// </summary>
+        public DbSet<RoleEntity> Roles { get; set; }
+        /// <summary>
+        /// Клиенты
+        /// </summary>
+        public DbSet<ClientEntity> Clients { get; set; }
+        /// <summary>
+        /// Счета
+        /// </summary>
+        public DbSet<CountEntity> Counts { get; set; }
+        /// <summary>
+        /// Транзакции
+        /// </summary>
+        public DbSet<TransactionEntity> Transactions { get; set; }
+        /// <summary>
+        /// Услуги/товары
+        /// </summary>
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
