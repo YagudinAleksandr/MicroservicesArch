a3a5f87fed3a8751c0a554761168bcaa10d84f06
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 16:16:53 +0300

    Базовый интерфейс сущности

---
+++ Infrastructure/MicroserviceArch.Interfaces/Entities/Base/IBaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MicroserviceArch.Interfaces.Entities.Base
+{
+    /// <summary>
+    /// Интерфейс базовой сущности
+    /// </summary>
+    public interface IBaseEntity
+    {
+        /// <summary>
+        /// ID сущности
+        /// </summary>
+        int Id { get; set; }
+        /// <summary>
+        /// Дата создания сущности
+        /// </summary>
+        DateTimeOffset CreatedAt { get; set; }
+        /// <summary>
+        /// Дата изменения сущности
+        /// </summary>
+        DateTimeOffset UpdatedAt { get; set; }
+    }
+}
