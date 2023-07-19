49c4925320d3fd41e6245e1cb8229630800ca07b
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 23:35:13 +0300

    Контроллер ролей

--- Services/MicroserviceArch.UsersService/Controllers/RolesController.cs
+++ Services/MicroserviceArch.UsersService/Controllers/RolesController.cs
@@ -1,7 +1,11 @@
 ﻿using MicroserviceArch.DAL.Entities;
+using MicroserviceArch.DTOEntity;
 using MicroserviceArch.Interfaces.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MicroserviceArch.UsersService.Controllers
@@ -18,9 +22,24 @@ namespace MicroserviceArch.UsersService.Controllers
         public RolesController(IRoleRepository<RoleEntity> repository) => this.repository = repository;
 
         [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Get(int id) => await repository.Get(id) is { } item ? Ok(item) : NotFound();
+        public async Task<IActionResult> Get(int id)
+        {
+            var role = await repository.Get(id);
+
+            if (role is null) return NotFound(new RoleEntityDTO { IsSuccesful = false, Notification = "Запись не найдена" });
+
+            RoleEntityDTO roleEntityDTO = new RoleEntityDTO
+            {
+                IsSuccesful = true,
+                Id = role.Id,
+                Name = role.Name,
+                CreatedAt = role.CreatedAt,
+                UpdatedAt = role.UpdatedAt,
+                Description = role.Description,
+            };
+
+            return Ok(roleEntityDTO);
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -28,33 +47,111 @@ namespace MicroserviceArch.UsersService.Controllers
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Add(RoleEntity item)
+        public async Task<IActionResult> Add([FromBody] RoleEntityDTO roleEntityDTO)
         {
-            var result = await repository.Add(item);
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    RoleEntity roleEntity = new RoleEntity()
+                    {
+                        Name = roleEntityDTO.Name,
+                        Description = roleEntityDTO.Description,
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow
+                    };
 
-            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+                    var result = await repository.Add(roleEntity);
+
+                    roleEntityDTO.Id = result.Id;
+                    roleEntityDTO.IsSuccesful = true;
+
+                    return Ok(roleEntityDTO);
+                }
+                else
+                {
+                    var errors = ModelState.Select(x => x.Value.Errors)
+                               .Where(y => y.Count > 0)
+                               .ToList();
+                    return BadRequest(new RoleEntityDTO { IsSuccesful = false, Notification = $"{errors}" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new RoleEntityDTO { IsSuccesful = false, Notification = $"{ex.Message}" });
+            }
         }
 
         [HttpPut]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Update(RoleEntity item)
+        public async Task<IActionResult> Update([FromBody]RoleEntityDTO item)
         {
-            var result = await repository.Update(item);
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    var errors = ModelState.Select(x => x.Value.Errors)
+                               .Where(y => y.Count > 0)
+                               .ToList();
+                    return BadRequest(new RoleEntityDTO { IsSuccesful = false, Notification = $"{errors}" });
+                }
 
-            if (result is null)
-                return NotFound(item);
-            return AcceptedAtAction(nameof(Get), new { id = result.Id }, result);
+                var role = await repository.Get(item.Id);
+
+                if(role is null)
+                {
+                    item.IsSuccesful = false;
+                    item.Notification = $"Не удалось найти роль {item.Name} с иднексом {item.Id} для изменения";
+                    return NotFound(item);
+                }
+
+                role.Description = item.Description;
+                role.UpdatedAt = DateTime.UtcNow;
+                role.Name = item.Name;
+
+                var result = await repository.Update(role);
+
+                item.Name = result.Name;
+                item.Description = result.Description;
+                item.UpdatedAt = result.UpdatedAt;
+                item.CreatedAt = result.CreatedAt;
+                item.IsSuccesful = true;
+
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new RoleEntityDTO { IsSuccesful = false, Notification = $"{ex.Message}" });
+            }
         }
 
         [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Delete(int id)
         {
-            if (await repository.Delete(id) is not { } result)
-                return NotFound(id);
-            return Ok(result);
+            try
+            {
+                var result = await repository.Delete(id);
+
+                if (result is null)
+                    return NotFound(new RoleEntityDTO
+                    {
+                        IsSuccesful = false,
+                        Notification = "Роль для удаления не найдена"
+                    });
+
+                return Ok(new RoleEntityDTO
+                {
+                    Name = result.Name,
+                    Description = result.Description,
+                    CreatedAt = result.CreatedAt,
+                    UpdatedAt = result.UpdatedAt,
+                    IsSuccesful = true,
+                    Id = result.Id
+                });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new RoleEntityDTO { IsSuccesful = false, Notification = $"{ex.Message}" });
+            }
         }
     }
 }
