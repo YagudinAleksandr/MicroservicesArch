cfdbd660dbc40210acb4723617b40074be4dc7ed
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 18:22:42 +0300

    Подключение к БД

--- Services/MicroserviceArch.UsersService/Startup.cs
+++ Services/MicroserviceArch.UsersService/Startup.cs
@@ -1,16 +1,11 @@
+using MicroserviceArch.DAL.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MicroserviceArch.UsersService
 {
@@ -26,6 +21,11 @@ namespace MicroserviceArch.UsersService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataDB>(options =>
+            options.UseNpgsql(Configuration
+                .GetConnectionString("VehicleQuotesContext"), m => m.MigrationsAssembly("MicroserviceArch.Dal.PGSQL"))
+                .UseSnakeCaseNamingConvention()
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
