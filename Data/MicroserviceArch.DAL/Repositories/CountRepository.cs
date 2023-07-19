ac465c87c1f091fa7387aabddd835fc24385aa7e
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 16:55:16 +0300

    Подготовка репозиториев

---
+++ Data/MicroserviceArch.DAL/Repositories/CountRepository.cs
@@ -0,0 +1,44 @@
+﻿using MicroserviceArch.DAL.Entities;
+using MicroserviceArch.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroserviceArch.DAL.Repositories
+{
+    public class CountRepository<T> : ICountRepository<T> where T : CountEntity
+    {
+        public Task<T> Add(T entity, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(int id, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int id, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll(CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllByUser(int userId, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/MicroserviceArch.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using MicroserviceArch.DAL.Entities;
+using MicroserviceArch.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroserviceArch.DAL.Repositories
+{
+    public class ProductRepository<T> : IProductRepository<T> where T : ProductEntity
+    {
+        public Task<T> Add(T entity, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(int id, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int id, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll(CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity, CancellationToken cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
