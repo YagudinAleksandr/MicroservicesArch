3fb8b2cd58b29baa9e6242b1eb121165d83f2497
Александр Ягудин <yagudin.a@outlook.com>
2023-07-20 00:16:05 +0300

    DTO модель клиентов

---
+++ Infrastructure/MicroserviceArch.DTOEntity/ClientEntityDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroserviceArch.DTOEntity
+{
+    /// <summary>
+    /// DTO модель клиента
+    /// </summary>
+    public class ClientEntityDTO
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage = "Поле Email должно быть заполнено")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Поле Пароль должно быть заполнено")]
+        public string Password { get; set; }
+        [Required (ErrorMessage = "Поле ФИО должно быть заполнено")]
+        public string Name { get; set; }
+        public int RoleId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Notification { get; set; }
+        public bool IsSuccesfull { get; set; }
+    }
+}
