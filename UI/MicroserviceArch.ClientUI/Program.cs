daea2250b29edd8f62d2d5a50e33bd21f06377cf
Александр Ягудин <yagudin.a@outlook.com>
2023-07-19 15:46:13 +0300

    Добавьте файлы проекта.

---
+++ UI/MicroserviceArch.ClientUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroserviceArch.ClientUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
