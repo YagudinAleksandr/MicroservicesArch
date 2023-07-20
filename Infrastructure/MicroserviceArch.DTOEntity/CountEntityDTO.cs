2107da9924c8f26b34dc70662da354a9091a2ca0
Александр Ягудин <yagudin.a@outlook.com>
2023-07-20 17:20:10 +0300

    DTO модель счетов

---
+++ Infrastructure/MicroserviceArch.DTOEntity/CountEntityDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroserviceArch.DTOEntity
+{
+    public class CountEntityDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Сумма не может быть пустым значением")]
+        [Range(0.0, 500000, ErrorMessage ="Сумма может быть в пределах от 0 до 500000")]
+        public double Count { get; set; }
+        [Required(ErrorMessage = "Счет не может быть без клиента")]
+        public int ClientId { get; set; }
+        public string Client { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Notification { get; set; }
+        public bool IsSuccessful { get; set; }
+    }
+}
