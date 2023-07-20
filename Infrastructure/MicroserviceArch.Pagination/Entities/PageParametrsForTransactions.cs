7e732866e66361ee22a89352981f4b3078bb1d35
Александр Ягудин <yagudin.a@outlook.com>
2023-07-21 01:17:15 +0300

    Добавлено расширение для постраничного вывода транзакций

---
+++ Infrastructure/MicroserviceArch.Pagination/Entities/PageParametrsForTransactions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MicroserviceArch.Pagination.Entities
+{
+    public class PageParametrsForTransactions : PageParametrs
+    {
+        /// <summary>
+        /// Id счета
+        /// </summary>
+        public int CountId { get; set; } 
+        /// <summary>
+        /// Дата начала сортировки
+        /// </summary>
+        public DateTime startDate { get; set; } = default;
+        /// <summary>
+        /// Дата окончания сортировки
+        /// </summary>
+        public DateTime endDate { get; set; } = default;
+        /// <summary>
+        /// Тип сортировки
+        /// </summary>
+        public bool IsSortStandart { get; set; } = default;
+    }
+}
