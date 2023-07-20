d0020441ff9c2ce11b5681094cad7db989c42e96
Александр Ягудин <yagudin.a@outlook.com>
2023-07-20 21:29:06 +0300

    DTO модель для валют

---
+++ Data/MicroserviceArch.DAL/Repositories/CurrencyRateDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace MicroserviceArch.DAL.Repositories
+{
+    /// <summary>
+    /// Модель DTO для валюты
+    /// </summary>
+    public class CurrencyRateDTO
+    {
+        public bool Success { get; set; }
+        public string Query { get; set; }
+        public string Info { get; set; }
+        public string Historical { get; set; }
+        public string Date { get; set; }
+        public double Result { get; set; }
+    }
+}
