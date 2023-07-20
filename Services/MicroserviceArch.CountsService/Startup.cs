51f62ebc3d0530415bbb0621a1c6cb6419a1355f
Александр Ягудин <yagudin.a@outlook.com>
2023-07-20 21:38:54 +0300

    Добавлен сервис HTTPClient

--- Services/MicroserviceArch.CountsService/Startup.cs
+++ Services/MicroserviceArch.CountsService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Net.Http;
 
 namespace MicroserviceArch.CountsService
 {
@@ -30,7 +31,10 @@ namespace MicroserviceArch.CountsService
                 .UseSnakeCaseNamingConvention()
             );
 
+            string urlToCurrencyConverter = $"https://api.exchangeratesapi.io/v1/latest?access_key={Configuration.GetSection("AccessKeyToExchangeratesapi").Value}";
+
             services.AddScoped(typeof(ICountRepository<>), typeof(CountRepository<>));
+            services.AddScoped(sp => new HttpClient() { BaseAddress = new System.Uri(urlToCurrencyConverter) });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
