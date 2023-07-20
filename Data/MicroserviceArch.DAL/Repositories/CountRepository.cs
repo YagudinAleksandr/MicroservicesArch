1b693fe683790abb81174f0fe5ae94cfb2a9ae3b
Александр Ягудин <yagudin.a@outlook.com>
2023-07-20 17:19:53 +0300

    Репозиторий счетов

--- Data/MicroserviceArch.DAL/Repositories/CountRepository.cs
+++ Data/MicroserviceArch.DAL/Repositories/CountRepository.cs
@@ -1,44 +1,97 @@
-﻿using MicroserviceArch.DAL.Entities;
+﻿using MicroserviceArch.DAL.Context;
+using MicroserviceArch.DAL.Entities;
 using MicroserviceArch.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MicroserviceArch.DAL.Repositories
 {
-    public class CountRepository<T> : ICountRepository<T> where T : CountEntity
+    public class CountRepository<T> : ICountRepository<T> where T : CountEntity, new()
     {
-        public Task<T> Add(T entity, CancellationToken cancel = default)
+        #region Поля и Свойства
+        private readonly DataDB db;
+        protected DbSet<T> Set { get; }
+        protected virtual IQueryable<T> Items => Set;
+        #endregion
+        public CountRepository(DataDB db)
         {
-            throw new NotImplementedException();
+            this.db = db;
+            this.Set = this.db.Set<T>();
         }
 
-        public Task<T> Delete(int id, CancellationToken cancel = default)
+        public async Task<T> Add(T entity, CancellationToken cancel = default)
         {
-            throw new NotImplementedException();
+            if (entity == null || entity.ClientId == 0) throw new ArgumentNullException();
+
+            var client = db.Clients.Where(i => i.Id == entity.ClientId).FirstOrDefault();
+
+            if (client == null) throw new ArgumentNullException();
+
+            entity.Client = client;
+
+            await db.AddAsync(entity, cancel).ConfigureAwait(false);
+
+            await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+
+            return entity;
         }
 
-        public Task<T> Get(int id, CancellationToken cancel = default)
+        public async Task<T> Delete(int id, CancellationToken cancel = default)
         {
-            throw new NotImplementedException();
+            var item = Set.Local.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+                item = await Set.Select(i => new T { Id = i.Id })
+                    .FirstOrDefaultAsync(x => x.Id == id, cancel)
+                    .ConfigureAwait(false);
+
+            if (item is null) return null;
+
+            db.Entry(item).State = EntityState.Deleted;
+
+            await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+
+            return item;
         }
 
-        public Task<List<T>> GetAll(CancellationToken cancel = default)
+        public async Task<T> Get(int id, CancellationToken cancel = default) =>
+            await Items.Include(c=>c.Client).FirstOrDefaultAsync(item => item.Id == id, cancel).ConfigureAwait(false);
+
+        public async Task<List<T>> GetAll(CancellationToken cancel = default) =>
+            await Items.Include(c => c.Client).ToListAsync(cancel).ConfigureAwait(false);
+
+        public async Task<List<T>> GetAllByUser(int userId, CancellationToken cancel = default) =>
+            await Items.Where(x => x.ClientId == userId).Include(c => c.Client).ToListAsync(cancel).ConfigureAwait(false);
+
+        public async Task<T> Update(T entity, CancellationToken cancel = default)
         {
-            throw new NotImplementedException();
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+
+            entity.Client = null;
+
+            db.Entry(entity).State = EntityState.Modified;
+
+            await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+
+            return entity;
         }
 
-        public Task<List<T>> GetAllByUser(int userId, CancellationToken cancel = default)
+        public async Task<bool> ExsistCount(int countId, CancellationToken cancel = default) =>
+            await Items.AnyAsync(item => item.Id == countId, cancel).ConfigureAwait(false);
+
+        public async Task<bool> CheckBalanceForTransaction(int countId, double sum, CancellationToken cancel = default)
         {
-            throw new NotImplementedException();
+            var count = await Items.FirstOrDefaultAsync(item => item.Id == countId, cancel).ConfigureAwait(false);
+
+            return count.Count >= sum ? true : false;
         }
 
-        public Task<T> Update(T entity, CancellationToken cancel = default)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> ExistsCounts(int clientId, CancellationToken cancel = default)=>
+            await Items.AnyAsync(item => item.ClientId == clientId, cancel).ConfigureAwait(false);
+        
     }
 }
